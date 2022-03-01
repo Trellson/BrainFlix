ec85f6696afb20ddd88ab5306e2bf60f27ea3c03
Artrell Coker <trellson13@gmail.com>
2022-03-01 05:41:19 -0500

    infosection created with icons

--- src/componets/video/currentVideoPlayer.js
+++ src/componets/video/currentVideoPlayer.js
@@ -1,25 +1,39 @@
 import "../Video/video.scss"
+import likes from '../../assets/Icons/likes.svg';
+import views from '../../assets/Icons/views.svg';
 
 
 
 export default function Video({currentVideo}){
 
+    console.log(currentVideo)
 
     return (
-        <div className="top-video">
-        <video className="hero-player" poster={currentVideo.image}>
-        </video>
-        <h2 className="player__title">{currentVideo.title}</h2>
-        <section className="player__description">
-            {currentVideo.description}
-        </section>
-        <section className="player__icons">
-            <div>
-                <p>likes {currentVideo.likes}</p>
+        <div className="hero" >
+            <video className="hero__player" poster={currentVideo.image}>
+                </video>
+            <section className="hero__container" >
+                <h2 className="hero__player--title">{currentVideo.title}</h2>
+            </section>
+
+            <section className="hero__player--icons">
+            <div className="channel--date">
+                <h4 className="channel"> {currentVideo.channel}</h4>
+                <p> {new Date(currentVideo.timestamp).toLocaleDateString()}</p>
 
             </div>
 
+            <div className="views--likes">
+                <div className="views"><img src={views} alt="views" /><p>{currentVideo.views}</p></div>
+                <div className="likes"><img src={likes} alt="likes" /><p>{currentVideo.likes}</p></div>
+            </div>
+
         </section>
+        
+        <section className="hero__player--description">
+            {currentVideo.description}
+        </section>
+        
         </div>
         
 
